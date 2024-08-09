04ca97746994e48956c4a36b1f85387918cb8be7
Ukam <ukam@Xubuntu.myguest.virtualbox.org>
2024-08-09 12:40:40 +0000

    Added routes

---
+++ routes/indexRouter.js
@@ -0,0 +1,36 @@
+
+const { Router } = require("express");
+const indexRouter = Router();
+
+
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+];
+
+indexRouter.get("/", (req, res) => {
+    res.render("index", {title:"Mini Message Board", mainMessages: messages});
+});
+
+indexRouter.get("/new", (req, res) => {
+    res.render("form");
+});
+
+
+indexRouter.post("/new", (req,res) => {
+    const Text = req.body.Text;
+    const User = req.body.User;
+
+    messages.push({ text: Text, user: User, added: new Date() });
+    res.redirect("/");
+});
+
+module.exports = indexRouter;
